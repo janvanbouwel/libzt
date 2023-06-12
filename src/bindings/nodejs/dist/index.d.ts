1e03ebf4abf468110ccf6e2fe49e1b896305a223
janvanbouwel <mrpetmans@gmail.com>
2023-06-12 23:06:23 +0200

    Add exports to index

--- src/bindings/nodejs/dist/index.d.ts
+++ src/bindings/nodejs/dist/index.d.ts
@@ -1,1 +1,4 @@
 export declare function init(path: string): void;
+export * from "./zts";
+export * from "./Socket";
+export * from "./Server";
--- src/bindings/nodejs/src/index.ts
+++ src/bindings/nodejs/src/index.ts
@@ -6,3 +6,7 @@ export function init(path: string) {
 
     zts.node_start();
 }
+
+export * from "./zts";
+export * from "./Socket";
+export * from "./Server";
